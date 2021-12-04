0cfb254fdc3279636fcc84ff0a274328a772b919
Rafael Perez <rbperez@uninorte.edu.co>
2021-12-03 20:21:37 -0500

    Eliminar producto

--- back/src/server.js
+++ back/src/server.js
@@ -11,11 +11,28 @@ app.get("/", function (req, res) {
     res.send("Bienvenidos al mundo dev!!")
 });
 
-//API - Consultar Producto
+/**
+ * API Rest Consultar Producto
+ * Descripción: Consulta un nuevo producto en la BD
+ * Ruta: /producto/consultar
+ * Método: POST
+ * Datos de entrada: {nombre: "papa"}
+ * Respuesta: {estado: "ok", msg: "Producto Guardado :)", data: {nombre: "papa", precio: 350, stock:130}}
+ */
 app.get("/producto/consultar/:name", function (req, res) {
+    // Define las variables necesarias
+    // Por defecto se falla
+    let mensaje = "NO encontrado";
+    let estado = "error";
+    //Captura el nombre del producto a Consultar
     const name = req.params.name;
     const prod = productos.find(p => p.title.toLowerCase() == name.toLowerCase());
-    res.send(prod);
+    // Producto Encontrado
+    if (prod != null && prod != undefined) {
+        mensaje = "Encontrado";
+        estado = "ok";
+    }
+    res.send({ estado, msg: mensaje, data: prod });
 });
 
 /**
@@ -32,7 +49,7 @@ app.post("/producto/guardar", function (req, res) {
     const prod = { title: nombre, price: precio, stock };
     productos.push(prod);
     console.log(productos);
-    res.send({estado: "ok", msg: "Producto Guardado :)"});
+    res.send({ estado: "ok", msg: "Producto Guardado :)" });
 })
 
 /**
@@ -50,14 +67,37 @@ app.post("/producto/editar", function (req, res) {
     // Buscar el producto a Editar
     let i = 0;
     for (const p of productos) {
-        if (p.title.toLowerCase() == nombre.toLowerCase()){
+        if (p.title.toLowerCase() == nombre.toLowerCase()) {
             productos[i].price = precio; // Editar precio
             productos[i].stock = stock; // Editar stock
             break;
         }
         i++;
     }
-    res.send({estado: "ok", msg: "Producto Editado :)"});
+    res.send({ estado: "ok", msg: "Producto Editado :)" });
+})
+
+/**
+ * API Rest Eliminar Producto
+ * Descripción: Elimina un producto guardado en BD
+ * Ruta: /producto/eliminar
+ * Método: POST
+ * Datos de entrada: {nombre: "papa"}
+ * Respuesta: {estado: "ok", msg: "Producto Eliminado"}
+ */
+app.post("/producto/eliminar", function (req, res) {
+    //Desestructuración
+    const { nombre } = req.body;    
+    // Buscar el producto a Editar
+    let i = 0;
+    for (const p of productos) {
+        if (p.title.toLowerCase() == nombre.toLowerCase()) {
+            productos.splice(i, 1);
+            break;
+        }
+        i++;
+    }
+    res.send({ estado: "ok", msg: "Producto Eliminado" });
 })
 
 app.listen(8080, () => {
